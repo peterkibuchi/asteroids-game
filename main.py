ae4aea8a632e23286d59ec5c3fc52dc3a9cd36c3
Peter Kibuchi <68213317+peterkibuchi@users.noreply.github.com>
2024-09-27 10:59:39 +0300

    Add Player class and draw player triangle on screen

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
@@ -14,12 +15,15 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    player_one = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while (True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         screen.fill((0, 0, 0))
+        player_one.draw(screen)
         pygame.display.flip()
 
         delta_time = clock.tick(60)
