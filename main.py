7e6010f9d58dae8f208033295a82b1a50c2871ec
Peter Kibuchi <68213317+peterkibuchi@users.noreply.github.com>
2024-09-27 06:39:07 +0300

    Refactor main loop to run 60 fps

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     while (True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -18,6 +21,9 @@ def main():
 
         screen.fill((0, 0, 0))
         pygame.display.flip()
+
+        delta_time = clock.tick(60)
+        dt = delta_time / 1000
 
 
 if __name__ == "__main__":
