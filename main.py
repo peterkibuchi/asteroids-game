bb89541da51a586cdc7779f30915d1f1512b4d71
Peter Kibuchi <68213317+peterkibuchi@users.noreply.github.com>
2026-01-19 12:34:22 +0300

    feat: implement shooting mechanics with Shot class and player shooting functionality

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from asteroidfield import AsteroidField
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_event, log_state
 from player import Player
+from shot import Shot
 
 
 def main():
@@ -21,10 +22,12 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
     Asteroid.containers = (updatable, drawable, asteroids)
     AsteroidField.containers = (updatable)
+    Shot.containers = (updatable, drawable, shots)
 
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
