078269fc05730854b2c0dd9326ed0b582d5c740c
Peter Kibuchi <68213317+peterkibuchi@users.noreply.github.com>
2026-01-19 10:54:05 +0300

    feat: implement CircleShape and Player classes with drawing functionality

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 
 def main():
@@ -13,6 +14,10 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
+
     while True:
         log_state()
 
@@ -21,6 +26,7 @@ def main():
                 return
 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
         # limits game to at most 60fps, computes & returns how much time has passed since prev call
