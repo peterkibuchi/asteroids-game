303ac964aece15b07060f101bd83e5ae749803c3
Peter Kibuchi <68213317+peterkibuchi@users.noreply.github.com>
2026-01-19 07:48:03 +0300

    feat: initialize clock and frame rate control

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         log_state()
 
@@ -19,6 +22,10 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()
+
+        # limits game to at most 60fps, computes & returns how much time has passed since prev call
+        dt_in_ms = clock.tick(60)
+        dt = dt_in_ms / 1000  # convert to seconds
 
 
 if __name__ == "__main__":
